cd11615e97cbfa9910e5c64319bb6d6d5bef2661
Mukesh Mahato <mukeshmahato2618@gmail.com>
2023-11-21 20:56:34 +0545

    terraform add

---
+++ Terraform with AWS/main.tf
@@ -0,0 +1,93 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.myvpc
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.myvpc
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "websg"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "mys3bucketforawsproject"
+}
+
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
